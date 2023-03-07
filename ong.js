2c5ce841a72df8386bf12dbc1f05f2af24b6d0fa
GiingsDev <54989424+GiingsDev@users.noreply.github.com>
2023-03-07 12:27:00 +0700

    Update ong.js (#9)

--- ong.js
+++ ong.js
@@ -8,12 +8,4 @@ if (x > 30) {
     answer =  "less than 40";
 }
 
-let variable1;
-let variable2 = variable1  || 'bar';
-console.log(variable2 === 'bar'); // prints true
-
-variable1 = 'foo';
-variable2 = variable1  || 'bar';
-console.log(variable2); // prints foo
-
 // changed
